f2c9994597f0065d58caa2261fe074ef9a409739
Junchi <benson26365156@gmail.com>
2025-01-18 17:07:09 +0800

    build: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config'
+
 import { message } from '@/example'
 
 import { getUsers } from './apis/example'
 
+console.log('NODE_ENV', process.env['NODE_ENV'])
 console.log(message)
 
 void getUsers().then((result) => {
