c77a1ec9a666b43eb7e4b796c6d8016abb8e321a
Junchi <benson26365156@gmail.com>
2025-02-02 12:27:58 +0800

    feat: use axios wrapper package

--- src/apis/example.ts
+++ src/apis/example.ts
@@ -1,6 +1,5 @@
+import { createApiClient } from '@chi0307/axios-wrapper'
 import typia from 'typia'
-
-import { createApiClient } from './client'
 
 const apiClient = createApiClient('https://5a019f7b-11a8-43f8-8dad-fc6d4303ee3d.mock.pstmn.io')
 
@@ -9,3 +8,7 @@ interface User {
   name: string
 }
 export const getUsers = apiClient.get('/users', typia.createIs<User[]>())
+export const getUser = apiClient.get<{ userId: string }, never, User>(
+  '/user/:userId',
+  typia.createIs<User>(),
+)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config'
 
+import { AxiosError } from 'axios'
+
 import { message } from '@/example'
 
 import { getUsers } from './apis/example'
@@ -7,6 +9,11 @@ import { getUsers } from './apis/example'
 console.log('NODE_ENV', process.env['NODE_ENV'])
 console.log(message)
 
-void getUsers().then((result) => {
-  console.log(result)
-})
+void getUsers()
+  .then((result) => {
+    console.log(result.data)
+  })
+  .catch((err: unknown) => {
+    console.error('err', err)
+    console.log('is AxiosError', err instanceof AxiosError)
+  })
