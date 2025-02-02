d65037616a075f0f5ce5a652661aec7ca5fc6712
Junchi <benson26365156@gmail.com>
2025-02-02 12:32:36 +0800

    build: test script and jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
-  },
   testPathIgnorePatterns: ['/node_modules/', '/dist/', '/typia-generated/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json',
+      },
+    ],
   },
   collectCoverage: true,
   coverageDirectory: 'coverage',
