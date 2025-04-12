9becd44f23f54c13e7f1d0fb29b3092334b4bea8
Junchi <benson26365156@gmail.com>
2025-04-12 15:20:22 +0800

    build: add check runtime

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
 import 'dotenv/config'
 
-import { AxiosError } from 'axios'
-
 import { getUsers } from '@/apis/example'
 import { message } from '@/example'
 
 console.log('NODE_ENV', process.env['NODE_ENV'])
 console.log(message)
 
-void getUsers()
-  .then((result) => {
-    console.log(result.data)
-  })
-  .catch((err: unknown) => {
-    console.error('err', err)
-    console.log('is AxiosError', err instanceof AxiosError)
-  })
+void getUsers().then((result) => {
+  console.log(result.data)
+})
